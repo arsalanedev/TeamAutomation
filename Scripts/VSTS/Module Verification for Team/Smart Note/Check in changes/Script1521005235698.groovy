dbae3791cea1868ac15dc8af27b672459477beac
muzzamil.qamar <muzzamil.qamar@Bilal.edevtech.com>
2018-03-14 11:41:36 +0500

    ds

--- Scripts/VSTS/Module Verification for Team/Smart Note/Check in changes/Script1521005235698.groovy
+++ Scripts/VSTS/Module Verification for Team/Smart Note/Check in changes/Script1521005235698.groovy
@@ -19,7 +19,3 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.acceptAlert()
-
-WebUI.check(findTestObject(null))
-
